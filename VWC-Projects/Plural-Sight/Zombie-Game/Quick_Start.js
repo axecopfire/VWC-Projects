4ec8dee9490854d98c968a475d3c1fa613b0edd0
axecopfire <braun.sn11@gmail.com>
2017-10-18 15:45:41 -0400

    Updated zombie-game to assignment 2

--- VWC-Projects/Plural-Sight/Zombie-Game/Quick_Start.js
+++ VWC-Projects/Plural-Sight/Zombie-Game/Quick_Start.js
@@ -1,5 +1,8 @@
 alert("The zombie apocalypse is going down! You are looting a store and suddenly the zombie bursts through the door.");     
 
+var setting = [
+    {backAlley: ["dumpster", "pizza", ""]}
+]
 var weapon = prompt("You look around frantically for a weapon. What do you choose? a can of soup, a jigsaw, or a spoon?");
 var randomNumber = Math.round(Math.random());
 
@@ -11,13 +14,3 @@ if(randomNumber === 0){
     alert("You kill the zombie with your" + " " + weapon + ". You survive!");
 }
 
-
-/*
-var MyName = "Schuster Braun "
-var age = 2017 - 1990;
-var city = " OC"
-var sentence = MyName + age + city;
-
-
-document.getElementById("solution").innerHTML = sentence;
-*/
