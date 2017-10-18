df24df54bc7a571ebbccfa4bc686041643c261e8
axecopfire <axecopfire@users.noreply.github.com>
2017-10-17 22:15:35 -0400

    vol 2 zombie updated

--- VWC-Projects/Plural-Sight/Zombie-Game/Quick_Start.js
+++ VWC-Projects/Plural-Sight/Zombie-Game/Quick_Start.js
@@ -1,23 +1,21 @@
-alert("The zombie apocalypse is going down! You are looting a store and suddenly the zombie bursts through the door.");     
+var setting = ["You are tired and lazy and roll over in your bed and a zombie is staring you in the face.", "You tried to go home after school one day but the bus driver was bitten by a zombie and now you have to walk. Along the way you are assailed by a murder of zombie crows.", "As you paddle out into the morning sun thinking you are going to catch the gnarliest wave set today. When from down below comes a zombie swordfish."];
 
-var weapon = prompt("You look around frantically for a weapon. What do you choose? a can of soup, a jigsaw, or a spoon?");
-var randomNumber = Math.round(Math.random());
-
-alert("You attack the zombie with your" + " " + weapon);
-
-if(randomNumber === 0){
-    alert("The zombie bites you. You die!");
-} else if (randomNumber === 1){
-    alert("You kill the zombie with your" + " " + weapon + ". You survive!");
+function randomNumber(range) {
+    return Math.round(Math.random() * range);
 }
 
+alert(setting[randomNumber(setting.length - 1)]);
 
-/*
-var MyName = "Schuster Braun "
-var age = 2017 - 1990;
-var city = " OC"
-var sentence = MyName + age + city;
+var weaponList = ["a pouch of Capri-Sun", "a baseball card", "a pair of keys", "a boombox"];
 
+var weapon = weaponList[randomNumber(weaponList.length - 1)];
 
-document.getElementById("solution").innerHTML = sentence;
-*/
+alert("Yo get outta here zombie! You pull " + weapon + " out and start waving it menacingly.");
+
+var survival = randomNumber(2)
+
+if (randomNumber === 0) {
+    alert("The zombie bites you. You die!");
+} else if (randomNumber > 0) {
+    alert("You kill the zombie with " + weapon + ". You survive!");
+}
