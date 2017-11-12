8afc285968d05b945c20249ddb32a8711165a0c9
axecopfire <axecopfire@users.noreply.github.com>
2017-11-12 12:01:02 -0500

    catalog finished, index updated with redirect, routes required in app

--- VWC-Projects/Express-app/express-locallibrary-tutorial/app.js
+++ VWC-Projects/Express-app/express-locallibrary-tutorial/app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
+var catalog = require('./routes/catalog'); //Import routes for "catalog" area of site
 
 var app = express();
 
