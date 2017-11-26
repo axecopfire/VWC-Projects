665c1a9357d9e99ed0ad0dd6615a2100ba8aa497
axecopfire <axecopfire@users.noreply.github.com>
2017-11-26 13:18:54 -0500

    compression added

--- VWC-Projects/Express-app/express-locallibrary-tutorial/app.js
+++ VWC-Projects/Express-app/express-locallibrary-tutorial/app.js
@@ -9,6 +9,7 @@ var expressValidator = require('express-validator');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var catalog = require('./routes/catalog');
+var compression = require('compression');
 
 var app = express();
 
@@ -39,6 +40,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cookieParser());
+
+app.use(compression()); // Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
