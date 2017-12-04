7b762234ea216ec7406545fcf467557536b4a6ae
axecopfire <axecopfire@users.noreply.github.com>
2017-12-03 21:08:18 -0500

    insertion sort finished

--- VWC-Projects/CS_Week/InsertionSort.js
+++ VWC-Projects/CS_Week/InsertionSort.js
@@ -1,1 +1,15 @@
 arr = [9, 1, 2, 7, 6, 4, 15, 123, 13]
+
+function insertionSort(items) {
+    for (var i = 0; i < items.length; i++) {
+        var temp = items[i];
+        while (i > 0 && items[i - 1] > temp) {
+            items[i] = items[i - 1];
+            i--;
+        }
+        items[i] = temp;
+    }
+    return items;
+}
+
+console.log(insertionSort(arr));
