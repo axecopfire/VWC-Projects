ac5102bc0f2c1966cbdf4ddacc20c049326edd69
axecopfire <axecopfire@users.noreply.github.com>
2017-12-04 23:00:22 -0500

    node-fun

---
+++ VWC-Projects/Node-Homework/node-fun/files.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+
+console.log('Executed Before File Reading.');
+
+fs.readFile('./file.txt', 'utf8', function(error, data) {
+    console.log(data);
+});
+
+console.log('Executed After File Reading.');
